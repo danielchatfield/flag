6ecaebce1b778018fdcd1d2b9e3fe0dd2917c449
Daniel Chatfield <chatfielddaniel@gmail.com>
2016-06-26 23:12:03 +0100

    making PyPI ready

---
+++ README.rst
@@ -0,0 +1,29 @@
+flag
+====
+
+A golang inspired simple flag library for python.
+
+Installation
+------------
+
+To install flag, simply:
+
+.. code-block:: bash
+
+    $ pip install flag
+
+
+Usage
+-----
+
+.. code-block:: python
+    import flag
+
+    num_workers = flag.int("workers", 10, "Number of worker threads")
+
+
+    if __name__ == '__main__':
+        flag.parse()
+
+    print("Num workers %d" % num_workers)
+```
--- setup.py
+++ setup.py
@@ -10,9 +10,15 @@ with open('flag/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
+with open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
+
 setup(
     name='Flag',
     version=version,
+    description="Simple flag library for python",
+    long_description=readme,
+    url='http://github.com/danielchatfield/flag',
     author='Daniel Chatfield',
     author_email='chatfielddaniel@gmail.com',
     packages=['flag'],
